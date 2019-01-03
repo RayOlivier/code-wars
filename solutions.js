8740ff8641652c0062e361b8d92ac1adbac49eac
Ray Olivier <rayolivier@outlook.com>
2019-01-02 18:00:54 -0600

    not sure what has been changed tbh

--- solutions.js
+++ solutions.js
@@ -130,6 +130,14 @@ function sumTwoSmallestNumbers(numbers) {
   return numbers[0] + numbers[1];
 }
 
+// In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+function filter_list(l) {
+  // Return a new array with the strings filtered out
+  return l.filter((e) => {
+    return Number.isInteger(e);
+  });
+}
+
 ////////////////////////////////////////////////////////////////////////////
 ////////////////////////// 6 KYU //////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////
